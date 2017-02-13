88e0a2b2ccf3192b87b9e5d95258b5570dbcbb2e
raitDBm <dyrait.db@gmail.com>
2017-02-13 23:39:18 +0530

    initial commit

---
+++ queryExecutor.php
@@ -0,0 +1,58 @@
+<?php
+//server connection script
+include 'dbconnect.php';
+
+//tag which is fetch from sender
+$tags=$_POST['tag'];
+
+//main logic
+switch ($tags)
+{
+case "createDatabase":
+
+    $DBName=$_POST['dbName'];
+    $sql = "CREATE DATABASE $DBName";
+    if ($conn->query($sql) === TRUE) {
+      echo "Database created successfully";
+    } else {
+      echo "Error creating database: " . $conn->error;
+    }
+    $conn->close();
+    break;
+
+case "selectDB":
+    $DBName=$_POST['dbName'];
+    if($conn->select_db($DBName))
+    {
+      echo "Database selected successfully..!";
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+
+case "createTable":
+    $DB_Name=$_POST['db_Name'];
+    $query_Stat=$_POST['Query'];
+    if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Table created successfully";
+      } else {
+          echo "Error creating table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+
+case "Drop":
+    echo "Drop";
+    break;
+default:
+    echo "Nothing to show";
+}
+
+?>
