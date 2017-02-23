4616e232911192457e11361546f2c0a9335413af
Mrunal-Gaikar <gaikar.mrunal@gmail.com>
2017-02-23 10:17:17 +0530

    Add files via upload
    dropping a column

--- queryExecutor.php
+++ queryExecutor.php
@@ -8,6 +8,23 @@ $tags=$_POST['tag'];
 //main logic
 switch ($tags)
 {
+case "DropColumn":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Column dropped successfully";
+      } else {
+          echo "Error dropping Column: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database does not exist..!";
+    }
+    break;
+
 case "createDatabase":
 
     $DBName=$_POST['dbName'];
@@ -48,8 +65,39 @@ case "createTable":
     }
     break;
 
-case "Drop":
-    echo "Drop";
+case "DropTable":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Table dropped successfully";
+      } else {
+          echo "Error dropping table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+	
+	
+case "DropDatabase":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Database dropped successfully";
+      } else {
+          echo "Error dropping Database: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database does not exist..!";
+    }
     break;
 default:
     echo "Nothing to show";
