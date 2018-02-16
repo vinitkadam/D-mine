b9925bacebd9cd97e47e06eb65c583791c672a31
vinitkadam <vinitkadam1997@gmail.com>
2018-02-16 13:03:33 +0530

    logs added

--- dbconnect.php
+++ dbconnect.php
@@ -7,6 +7,7 @@ $DbName = $_SESSION["roll_no"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $DbName);
+$con = new mysqli($servername, $username, $password, "dmine");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
